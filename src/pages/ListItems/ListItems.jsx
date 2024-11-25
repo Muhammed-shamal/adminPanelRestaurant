5bec8a84fab1d67b1705dbc78370c357034b714e
Ramees <rameesc985@gmail.com>
2024-11-25 08:43:51 +0530

    create admin page design

--- src/pages/ListItems/ListItems.jsx
+++ src/pages/ListItems/ListItems.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import './ListItems.css'
 
 function ListItems() {
   return (
-    <div>ListItems</div>
+    <div className='list add flex-col'>
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        <div className="list-table-format">
+          <img src="" alt="" />
+          <p>name</p>
+          <p>category</p>
+          <p>$price</p>
+          <p className='cursor'>x</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import './Orders.css'
 
+import {assets} from '../../assets/assets'
+
 function Orders() {
   return (
-    <div>Orders</div>
+    <div className='order add'>
+      <h3>Order Page</h3>
+      <div className="order-list">
+        <div className="order-item">
+          <img src={assets.parcel_icon} alt="" />
+          <div>
+            <p className='order-item-food'>
+              item name
+            </p>
+            <p>
+              order address
+            </p>
+            <div className='order-item-address'>
+              <p>street</p>
+              <p>city</p>
+            </div>
+            <p className="order-item-phone">
+              phone
+            </p>
+          </div>
+          <p>orderitems length</p>
+          <p>$amount</p>
+          <select>
+            <option value="Food Processing">Food Processing</option>
+            <option value="Out For Delivery">Out For Delivery</option>
+            <option value="Delivered">Delivered</option>
+          </select>
+        </div>
+      </div>
+    </div>
   )
 }
 
